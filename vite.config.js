b9d38f3462dd8bd0fa2324dca7cdd2fff0c3dc3b
Najib Julianto <najib.julianto@gt-tires.com>
2025-08-07 14:42:03 +0700

    franken

--- vite.config.js
+++ vite.config.js
@@ -18,24 +18,15 @@ export default defineConfig({
     alias: {
     }
   },
-  
-  // server: {
-  //   cors: true,
-  //   host: '0.0.0.0',
-  //   port: 5173,
-  //   https: {
-  //           key: fs.readFileSync('/var/www/nginx/ssl/server.key'),
-  //           cert: fs.readFileSync('/var/www/nginx/ssl/server.crt'),
-  //       },
-  //   strictPort: true,
-  //   watch: {
-  //     usePolling: true
-  //   },
-  //   origin: 'https://10.129.78.59:5173',
-  //   hmr: {
-  //     host: '10.129.78.59',
-  //     port: 5173,
-  //   },
-  // }
+
+  server: {
+    cors: true,
+    host: "0.0.0.0",
+    port: 5173,
+    https: {
+      key: fs.readFileSync('docker/certs/key.pem'),
+      cert: fs.readFileSync('docker/certs/cert.pem'),
+    },
+  }
 
 });
